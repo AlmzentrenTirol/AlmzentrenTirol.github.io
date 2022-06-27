ba3487f209fae55c1e7f024932324a0f1c9e0a2c
Mario Lehmeyer <mario.lehmeyer@student.uibk.ac.at>
2022-06-27 14:47:40 +0200

    weltweite Wettervorhersage inklusive Popups und Beschreibung hinzugefügt

--- kartemain.js
+++ kartemain.js
@@ -127,7 +127,58 @@ async function loadWind(url) {
 loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europe.json");
 
 // Wettervorhersage
-async function loadWeather(url) {
+layerControl.addOverlay(overlays.weather, "Wettervorhersage met.no");
 
+let marker = L.circleMarker([
+    47.267222, 11.392778
+]).bindPopup("Wettervorhersage").addTo(overlays.weather);
+
+async function loadWeather(url) {
+    const response = await fetch(url);
+    const jsondata = await response.json();
+
+       // Marker positionieren
+       marker.setLatLng([
+        jsondata.geometry.coordinates[1],
+        jsondata.geometry.coordinates[0]
+    ]);
+
+    let details = jsondata.properties.timeseries[0].data.instant.details;
+
+    let forecastDate = new Date(jsondata.properties.timeseries[0].time);
+
+    let forecastLabel = formatDate(forecastDate);
+
+    let popup = `
+        <strong>Wettervorhersage für ${forecastLabel}</strong>
+        <ul>
+            <li>Luftdruck: ${details.air_pressure_at_sea_level} (hPa)</li>
+            <li>Luftemperatur: ${details.air_temperature} (°C)</li>
+            <li>Bewölkung: ${details.cloud_area_fraction} (%)</li>
+            <li>Niederschlag: ${details.precipitation_amount} (mm)</li>
+            <li>Relative Luftfeuchtigkeit: ${details.relative_humidity} (%)</li>
+            <li>Windrichtung: ${details.wind_from_direction} (°)</li>
+            <li>Windgeschwindigkeit: ${(details.wind_speed * 3.6).toFixed(1)} (km/h)</li>
+        </ul>
+    `;
+
+       // Wettericons
+       for (let i=0; i <= 24; i+=3) {
+        let symbol = jsondata.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
+        let forecastDate = new Date(jsondata.properties.timeseries[i].time);
+        let forecastLabel = formatDate(forecastDate);
+
+        popup += `<img src="wettericons/${symbol}.svg" title="${forecastLabel}" alt="${symbol}" style="width:32px">`;
+    }
+
+    marker.setPopupContent(popup).openPopup();
 };
 loadWeather("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
+
+// auf Klick auf die Karte reagieren
+map.on("click", function(evt) {
+
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`;
+
+    loadWeather(url);
+});
